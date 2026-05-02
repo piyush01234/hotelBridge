43ca053a5b884fbc31e604e949c775d31c92ca00
piyush01234 <piyushmunjal31@gmail.com>
2026-05-02 16:30:32 +0530

    Add files via upload

---
+++ src/main/java/com/hotelbridge/HotelBridge/HotelBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.hotelbridge.HotelBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelbridge/HotelBridge/HotelBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelbridge.HotelBridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
